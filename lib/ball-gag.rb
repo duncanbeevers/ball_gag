68b5393e60291083ba7f39168c8ca9296ebe6036
Duncan Beevers <duncan@dweebd.com>
2011-10-09 23:04:02 -0700

    BallGag adds and removes attribute_gagged? methods

--- lib/ball-gag.rb
+++ lib/ball-gag.rb
@@ -2,25 +2,66 @@ require 'ball-gag/version'
 
 module BallGag
   def self.included base
-    @inclusions ||= {}
-    if !@inclusions[base]
-      @inclusions[base] = true
+    @gagged_inclusions ||= {}
+    if !@gagged_inclusions[base]
+      @gagged_inclusions[base] = true
       base.extend ClassMethods
     end
   end
 
   module ClassMethods
     def gagged_attributes
-      @gagged_attributes || clear_gagged_attributes
+      clear_gagged_attributes unless @gagged_attributes
       @gagged_attributes.keys
     end
 
-    def gag attribute
-      @gagged_attributes[attribute] = true
+    def gag attribute, &block
+      define_and_mixin_gagged_attributes_methods
+      @gagged_attributes[attribute] = block || lambda { }
+
+      define_gagged_interpellation attribute, @gagged_attributes[attribute]
+      define_not_gagged_interpellation attribute
     end
 
     def clear_gagged_attributes
+      undefine_gagged_attributes_methods if @gagged_attributes
       @gagged_attributes = {}
+    end
+
+    private
+    def define_and_mixin_gagged_attributes_methods
+      return if @gagged_attirbutes_methods
+
+      undefine_gagged_attributes_methods
+      include @gagged_attributes_methods
+    end
+
+    def define_gagged_interpellation attribute, block
+      @gagged_attributes_methods.send(:define_method, gagged_attribute_interpellation_name(attribute)) do
+        @gagged_attributes[attribute].call(self.send(attribute), self)
+      end
+    end
+
+    def define_not_gagged_interpellation attribute
+      @gagged_attributes_methods.send(:define_method, gagged_attribute_negative_interpellation_name(attribute)) do
+        !method(gagged_attribute_interpellation_name(attribute)).call
+      end
+    end
+
+    def gagged_attribute_interpellation_name attribute
+      "#{attribute}_gagged?"
+    end
+
+    def gagged_attribute_negative_interpellation_name attribute
+      "#{attribute}_not_gagged?"
+    end
+
+    def undefine_gagged_attributes_methods
+      @gagged_attributes.keys.each do |attribute|
+        @gagged_attributes_methods.send(:remove_method, gagged_attribute_interpellation_name(attribute))
+        @gagged_attributes_methods.send(:remove_method, gagged_attribute_negative_interpellation_name(attribute))
+      end
+      @gagged_attributes_methods = Module.new
     end
   end
 end
--- spec/ball-gag_spec.rb
+++ spec/ball-gag_spec.rb
@@ -2,18 +2,43 @@ require 'spec_helper'
 
 describe 'Plain Old Ruby Object' do
   it 'should have no gagged attributes' do
-    ActiveModelExample.gagged_attributes.should be_empty
+    ExampleModel.gagged_attributes.should be_empty
   end
 
   it 'should gag attribute' do
-    ActiveModelExample.gag :words
-    ActiveModelExample.gagged_attributes.should include :words
+    ExampleModel.gag :words
+    ExampleModel.gagged_attributes.should include :words
   end
 
-  it 'should clear gagged attributes' do
-    ActiveModelExample.gag :words
-    ActiveModelExample.clear_gagged_attributes
-    ActiveModelExample.gagged_attributes.should == []
+  describe 'when clearing gagged attributes' do
+    it 'should clear gagged attributes' do
+      ExampleModel.gag :words
+      ExampleModel.clear_gagged_attributes
+      ExampleModel.gagged_attributes.should == []
+    end
+
+    it 'should remove attribute_not_gagged? method' do
+      ExampleModel.gag :words
+      ExampleModel.new.should respond_to(:words_not_gagged?)
+      ExampleModel.clear_gagged_attributes
+      ExampleModel.new.should_not respond_to(:words_not_gagged?)
+    end
+
+    it 'should remove attribute_gagged? method' do
+      ExampleModel.gag :words
+      ExampleModel.new.should respond_to(:words_gagged?)
+      ExampleModel.clear_gagged_attributes
+      ExampleModel.new.should_not respond_to(:words_gagged?)
+    end
+  end
+
+  it 'should add new attribute_gagged? method' do
+    ExampleModel.new.should respond_to :words_gagged?
+  end
+
+  it 'should add new attribute_not_gagged? method' do
+    ExampleModel.new.should respond_to :words_not_gagged?
+  end
   end
 end
 
