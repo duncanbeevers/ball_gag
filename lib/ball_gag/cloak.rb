deea303183f50be79430622000d2f7340e90f47b
Duncan Beevers <duncan@dweebd.com>
2011-11-08 10:19:06 -0800

    Custom verb delegates to gag

--- lib/ball_gag/cloak.rb
+++ lib/ball_gag/cloak.rb
@@ -5,7 +5,18 @@ module BallGag
     end
 
     def verb= verb
+      old_verb = @verb
       @verb = verb
+
+      BallGag::ClassMethods.instance_eval do
+        if verb
+          define_method verb do |*args|
+            gag *args
+          end
+        elsif old_verb
+          remove_method old_verb
+        end
+      end
     end
   end
 
--- spec/cloak_spec.rb
+++ spec/cloak_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe 'BallGag cloaking' do
-  before { BallGag.verb = nil }
+  before do
+    BallGag.verb = nil
+    ExampleModel.clear_gagged_attributes
+  end
 
   it 'should have default verb' do
     BallGag.verb.should eq 'gag'
@@ -10,6 +13,23 @@ describe 'BallGag cloaking' do
   it 'should accept new verb' do
     BallGag.verb = 'censor'
     BallGag.verb.should eq 'censor'
+  end
+
+  it 'should provide verb as class method' do
+    BallGag.verb = 'censor'
+    ExampleModel.should respond_to :censor
+  end
+
+  it 'should gag attribute when custom verb is used' do
+    BallGag.verb = 'censor'
+    ExampleModel.censor :words
+    ExampleModel.gagged_attributes.should include :words
+  end
+
+  it 'should remove custom verb method when verb is cleared' do
+    BallGag.verb = 'censor'
+    BallGag.verb = nil
+    ExampleModel.should_not respond_to :censor
   end
 end
 
